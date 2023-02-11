34e88922b61c4f189b4b0263c894d98f3ad215d2
Ilya <sizov.ilya1996@gmail.com>
2023-02-11 14:20:10 +0300

    new update

--- nest/src/views/news/news-one.ts
+++ nest/src/views/news/news-one.ts
@@ -1,16 +1,15 @@
 import { CreateNewsDto } from '../../news/dto/create-news.dto';
 import { newUrl } from './news-all';
-import { CommentDto } from '../../news/comments/dto/comment.dto';
+import { CommentWithReplyDto } from '../../news/comments/dto/comment.dto';
 
 export function renderNewsOne(
   news: CreateNewsDto,
-  comments: CommentDto[] = undefined,
+  comments: CommentWithReplyDto[],
 ): string {
-  let renderComments = '';
-  if (comments) {
-    renderComments = renderCommentsAll(comments);
-  }
-  return `
+  let commentHtml = '';
+  commentHtml = renderCommentsAll(news, comments);
+
+  const html = `
 <div class='container'>
 <a href='${newUrl}' style='margin-bottom: 50px' class='btn btn-primary'>Вернуться назад</a>
   <div class='card' style='width: 100%'>
@@ -25,41 +24,111 @@ export function renderNewsOne(
       <p class='card-text'>${news.description}</p>
     </div>
   </div>
-  
-  ${
-    comments
-      ? `
-  <ul class='list-group' style='margin-top: 40px'>
-      ${renderComments}
-  </ul>`
-      : '<p card-text>Комментариев нет</p>'
-  }
-  
+${commentHtml}
   </div>
   `;
+  return html;
 }
 
-function renderCommentsAll(comments: CommentDto[]) {
-  let commentListHtml = '';
-  for (const commentItem of comments) {
-    commentListHtml += renderComment(commentItem);
+function renderCommentsAll(news, comments: CommentWithReplyDto[]) {
+  let commentListHtml = `
+  <div>
+      <p>Добавить комментарий:</p>
+      <form id='comment'>
+       <input type='text' name='author' id='author' placeholder='Имя' required/>
+       <input type='text' name='message' id='message' placeholder='Сообщение' required/>
+      <button>Отправить</button>
+       </form>
+       <script>
+       const form = document.getElementById('comment')
+       form.addEventListener('submit', (event) => {
+         const {author, message} = event.target.elements;
+              const myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+                const urlencoded = new URLSearchParams();
+                urlencoded.append("author", author.value);
+                urlencoded.append("message", message.value);
+                const requestOptions = {
+                  method: 'POST',
+                  headers: myHeaders,
+                  body: urlencoded,
+                  redirect: 'manual'
+                };
+                fetch("http://localhost:3000/api/comments/${news.id}", requestOptions)
+                  .then(response => response.text())
+                  .then(result => console.log(result))
+                  .catch(error => console.log('error', error));
+       })
+</script>
+    </div>
+    <div>
+    <h5>Комментарии:</h5>
+</div>
+  `;
+  if (!comments) {
+    commentListHtml += `
+    <p>Комментариев пока нет</p> `;
+  } else {
+    for (const commentItem of comments) {
+      commentListHtml += renderComment(news, commentItem);
+    }
+  }
+  return commentListHtml;
+}
+
+function renderComment(news, comment: CommentWithReplyDto) {
+  let commentReplyHtml = `
+  <div class='card' style='width: 100%'>
+  <div class='card-body'>
+      <h5 class='card-title'>Ответы:</h5>
+      <form id='comment-reply'>
+       <input type='text' name='author' id='author' placeholder='Имя'/>
+       <input type='text' name='message' id='message' placeholder='Сообщение'/>
+      <button>Отправить</button>
+       </form>
+       <script>
+       const formReply = document.getElementById('comment-reply')
+       formReply.addEventListener('submit', (event) => {
+         const {author, message} = event.target.elements;
+              const myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+                const urlencoded = new URLSearchParams();
+                urlencoded.append("author", author.value);
+                urlencoded.append("message", message.value);
+                const requestOptions = {
+                  method: 'POST',
+                  headers: myHeaders,
+                  body: urlencoded,
+                  redirect: 'manual'
+                };
+                fetch("http://localhost:3000/api/comments/${news.id}/${comment.id}", requestOptions)
+                  .then(response => response.text())
+                  .then(result => console.log(result))
+                  .catch(error => console.log('error', error));
+       })
+</script>
+   
+  `;
+  if (!comment.reply) {
+    commentReplyHtml += `
+    <p class='card-text'>Ответов нет</p>
+    `;
+  } else {
+    for (const commentReply of comment.reply) {
+      commentReplyHtml += `
+      <p class='card-text'>Автор: ${commentReply.author}</p>
+      <p class='card-text'>Сообщение: ${commentReply.message}</p>
+      `;
+    }
+    commentReplyHtml += `</div>`;
   }
 
-  return `<h3>Комментарии</h3>
-<div class='container'>
-    ${commentListHtml}
-</div>
-`;
-}
-
-function renderComment(comment: CommentDto) {
   return `
-  <li class='list-group-item'>
   <div class='card' style='width: 100%'>
-  <div class='card-body' data-id='${comment.id}'>
-   <h6 class='card-subtitle mb-2 text-muted'>${comment.author}</h6>
-    <p class='card-text'>${comment.message}</p>
+    <div class='card-body' data-id='${comment.id}'>
+     <h6 class='card-subtitle mb-2 text-muted'>Автор: ${comment.author}</h6>
+     <p class='card-text'>Сообшение: ${comment.message}</p>
+     ${commentReplyHtml}
     </div>
-    </div>
-</li>`;
+  </div>`;
 }
