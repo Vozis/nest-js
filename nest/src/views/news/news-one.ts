ca8929bf2a859942d7256dc9d85d4ddd9a737be3
Ilya <sizov.ilya1996@gmail.com>
2023-02-11 13:15:36 +0300

    update homework -2

--- nest/src/views/news/news-one.ts
+++ nest/src/views/news/news-one.ts
@@ -1,16 +1,31 @@
 import { CreateNewsDto } from '../../news/dto/create-news.dto';
 import { newUrl } from './news-all';
-import { CommentDto } from '../../news/comments/dto/comment.dto';
+import {
+  CommentDto,
+  CommentWithReplyDto,
+} from '../../news/comments/dto/comment.dto';
 
 export function renderNewsOne(
   news: CreateNewsDto,
-  comments: CommentDto[] = undefined,
+  comments: CommentWithReplyDto[],
 ): string {
-  let renderComments = '';
-  if (comments) {
-    renderComments = renderCommentsAll(comments);
+  let commentHtml = '';
+  if (!comments) {
+    commentHtml = `
+    <div>
+      <p>Добавить комментарий</p>
+      <form id='comment' action='http://localhost:3000/api/comments/${news.id}' method='POST'>
+       <input type='text' name='author' id='author' placeholder='Имя'/>
+       <input type='text' name='message' id='message' placeholder='Сообщение'/>
+      <button>Отправить</button>
+       </form>
+    </div>
+    `;
+  } else {
+    commentHtml = renderCommentsAll(news, comments);
   }
-  return `
+
+  const html = `
 <div class='container'>
 <a href='${newUrl}' style='margin-bottom: 50px' class='btn btn-primary'>Вернуться назад</a>
   <div class='card' style='width: 100%'>
@@ -25,41 +40,57 @@ export function renderNewsOne(
       <p class='card-text'>${news.description}</p>
     </div>
   </div>
-  
-  ${
-    comments
-      ? `
-  <ul class='list-group' style='margin-top: 40px'>
-      ${renderComments}
-  </ul>`
-      : '<p card-text>Комментариев нет</p>'
-  }
-  
+${commentHtml}
   </div>
   `;
+  return html;
 }
 
-function renderCommentsAll(comments: CommentDto[]) {
+function renderCommentsAll(news, comments: CommentWithReplyDto[]) {
   let commentListHtml = '';
   for (const commentItem of comments) {
-    commentListHtml += renderComment(commentItem);
+    commentListHtml += renderComment(news, commentItem);
   }
-
-  return `<h3>Комментарии</h3>
-<div class='container'>
+  return `
+<h3>Комментарии</h3>
+<div class='card'>
     ${commentListHtml}
 </div>
 `;
 }
 
-function renderComment(comment: CommentDto) {
-  return `
-  <li class='list-group-item'>
+function renderComment(news, comment: CommentWithReplyDto) {
+  let commentReplyHtml = `
   <div class='card' style='width: 100%'>
-  <div class='card-body' data-id='${comment.id}'>
-   <h6 class='card-subtitle mb-2 text-muted'>${comment.author}</h6>
-    <p class='card-text'>${comment.message}</p>
+  <div class='card-body'>
+      <h5 class='card-title'>Ответы:</h5>
+  `;
+  if (!comment.reply) {
+    commentReplyHtml = `
+    <p class='card-text'>Ответов нет</p>
+    <p>Добавить ответ</p>
+      <form id='comment' action='http://localhost:3000/api/comments/${news.id}/${comment.id}' method='POST'>
+       <input type='text' name='author' id='author' placeholder='Имя'/>
+       <input type='text' name='message' id='message' placeholder='Сообщение'/>
+      <button>Отправить</button>
+       </form>
+    `;
+  } else {
+    for (const commentReply of comment.reply) {
+      commentReplyHtml += `
+      <p class='card-text'>${commentReply.author}</p>
+      <p class='card-text'>${commentReply.message}</p>
+      `;
+    }
+    commentReplyHtml += `</div>`;
+  }
+
+  return `
+  <div class='card' style='width: 100%'>
+    <div class='card-body' data-id='${comment.id}'>
+     <h6 class='card-subtitle mb-2 text-muted'>${comment.author}</h6>
+     <p class='card-text'>${comment.message}</p>
+     ${commentReplyHtml}
     </div>
-    </div>
-</li>`;
+  </div>`;
 }
